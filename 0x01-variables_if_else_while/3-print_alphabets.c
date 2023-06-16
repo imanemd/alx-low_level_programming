483e0c4df14e0c82df20e670a0afb1defbd980cc
imanemd <imanemezdour6@gmail.com>
2023-06-16 17:18:42 +0000

    my commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point of the program
+ *Description: the variable n is positive or negative
+ *Return: 0 on successful program execution.
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
