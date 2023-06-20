f35dbcfe97e957adb4e38463a22fd9729225ae79
imanemd <imanemezdour6@gmail.com>
2023-06-20 12:34:57 +0000

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *main - Entry point of the program
+  *Description: the variable n is positive or negative
+  *Return: 0 on successful program execution.
+  */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
